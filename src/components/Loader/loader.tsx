b58f8b93b0ccf834f76024ebd1d66f0339694a0d
Melih Işık <melih.isik@solutions.consulting>
2021-12-07 16:16:37 +0300

    loading companies

--- src/components/Loader/loader.tsx
+++ src/components/Loader/loader.tsx
@@ -3,11 +3,13 @@ import BlockUi from "react-block-ui";
 import "react-block-ui/style.css";
 import { useAppSelector } from "../../app/hooks";
 import { itemsStatus } from "../../features/items/itemSlice";
+import { companyStatus } from "../../features/companies/companySlice";
 function Loader(props: any) {
   const itemsLoading = useAppSelector(itemsStatus);
+  const companyLoading = useAppSelector(companyStatus);
 
   return (
-    <BlockUi tag="div" blocking={itemsLoading || false}>
+    <BlockUi tag="div" blocking={itemsLoading || companyLoading}>
       {props.children}
     </BlockUi>
   );
